6e7fa6f671132d159a6e25cb6ecb8b4ba007540b
Your Name <priyavb@gmail.com>
2024-07-02 14:09:27 -0700

    file update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,54 +1,67 @@
-import os, csv
+import csv
+import os
 
-def CalculateChange(startingValue, endingValue):
-    return (endingValue - startingValue) #calculating the difference between the ending value and the starting value
 
-filePath = "C:\\Users\\nprab\\Downloads\\Starter_Code (2)\\Starter_Code\\PyBank\\Resources\\budget_data.csv"
+#files I need to load
+csvpath = "C:\\Users\\nprab\\Downloads\\Starter_Code (2)\\Starter_Code\\PyBank\\Resources\\budget_data.csv"
 
-#filePath = os.path.join('resources', 'budget_data.csv')#import csv block of code
+pathout = "C:\\Users\\nprab\\OneDrive\\Desktop\\repos\\python-challenge\\PyBank\\analysis\\budget_analysis.txt"
 
-with open(filePath) as csvfile: #open the path to the CSV file as a new object
-    csvreader = csv.reader(csvfile, delimiter=',') #pass the csvfile object to a new variable, csvreader
-    csv_header = next(csvreader) #skipping the header so we get the correct count of rows
-    print("Financial Analysis")
-    print("----------------------------")
-    rowCounter, total, avgChange, maxIncreaseAmount, maxIncreaseMonth, minIncreaseAmount, minIncreaseMonth  = 0, 0, 0, 0, 0, 0, 0
-    monthlyValues = [] #declaring empty list to keep track of individual monthly profit/loss values from the CSV
-    monthlyChangeAmount = [] #declaring empty list to keep track of the change in profit/loss across each month
-    monthlyChangeMonth = [] #declaring empty list to keep track of the month corresponding to the amount of the change in profit/loss
 
-    for row in csvreader:#Read each row of data after the header
-        monthlyValues.append(row[1]) #appending value of Profit/Loss column of the CSV to the monthlyValues list
-        total += int((row[1])) #calculating total amount of profit/loss over the entire period
-        #passing two values to CalculateChange function. the first parameter, startingValue, will be equal to the previous value of the iterator of the loop 
-        #which would be the current position in the list monthlyValues - 1. the second parameter, endingValue, will be equal to the current value of the 
-        # iterator of the loop. this value is then added to my empty list titled "monthlyChangeAmount"
+#pathout = os.path.join("Resources", "budget_analysis.txt")
 
-        monthlyChangeAmount.append(CalculateChange(int(monthlyValues[rowCounter-1]), int(row[1])))
-        monthlyChangeMonth.append(row[0]) #keep track of the months so I can map the name of the month to the value of the change for that month
-        #keeping track of the row count for two reasons: 1. to be able to get the proper index for my list 
-        #and 2. to count the number of months of data (each month has its own row in the CSV) 
-        rowCounter +=1 
-    #calculating average by taking the sum of the list divided by the length of the list. subtracing one from the length because the value from the first iteration 
-    #of the loop will be discarded since the value is zero as there is no prior value to calculate amount change to month one. rounding result to 2 decimal places.
-    avgChange =  round(sum(monthlyChangeAmount) / (len(monthlyChangeAmount)-1),2) 
-    maxIncreaseAmount = max(monthlyChangeAmount) #maximum value from the monthlyChangeAmount list
-    minIncreaseAmount = min(monthlyChangeAmount) #minimum value from the monthlyChangeAmount list
-    #getting the index of the max value from the Amounts list and using that value as the index for the Months list since they should be 
-    #at the same position since they correspond to the same row in the CSV
-    maxIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(max(monthlyChangeAmount))]
-    minIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(min(monthlyChangeAmount))]
+#total month needs to be set to 0 so I can count 
+totalMonth = 0
+totalRevenue = 0
+previousRevenue = 0
+revenue_change = 0
+revenue_change_list = []
+month_of_change = []
+greatestIncrease = ["", 0]
+greatestDecrease = ["", 1]
 
-def GenerateResults(resultType):#function to print the results to terminal or export the results to a text file
-    if (resultType == "print"):#print results to terminal
-        print(f"Total Months: {rowCounter}\nTotal: ${total}\nAverage Change: ${avgChange}\nGreatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})\nGreatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
-    elif(resultType == "file"):#export the same information to a text file
-       
-        #output_path = os.path.join("output", "financialResults.txt")
+#Read the budget_data.csv file
+with open(csvpath) as revenueData:
+   reader = csv.DictReader(revenueData)
+
+#I need to loop through the data to collect the answers
+   for col in reader:
+
+       #Totaling
+           totalMonth = totalMonth + 1
+           totalRevenue = totalRevenue + int(col["Profit/Losses"])
+
+#changes of revenue calculations
+           revenue_change = int(col["Profit/Losses"]) - previousRevenue
+           revenue_change_list.append(revenue_change)
+           previousRevenue = int(col["Profit/Losses"])
+           month_of_change = month_of_change + [col["Date"]]
+
+           #Greatest Increase value
+           if (revenue_change > greatestIncrease[1]):
+               greatestIncrease[1] = revenue_change
+               greatestIncrease[0] = col["Date"]
+
+           if (revenue_change < greatestDecrease[1]):
+               greatestDecrease[0] = col["Date"]
+               greatestDecrease[1] = revenue_change
         
-        output_path= "C:\\Users\\nprab\\OneDrive\\Desktop\\repos\\python-challenge\\PyBank\\analysis\\output.txt"
-        with open(output_path, 'w') as txtFile:
-            txtFile.write(f"Total Months: {rowCounter}\nTotal: ${total}\nAverage Change: ${avgChange}\nGreatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})\nGreatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
+#average revenue
+revenue_avg = sum(revenue_change_list) / len(revenue_change_list)
 
-GenerateResults("print")
-GenerateResults("file")       
+
+#print the outcomes
+output = (
+    f"Total Months: {totalMonth}\n"
+    f"Total Revenue: {totalRevenue}\n"
+    f"Average Revenue Change: ${revenue_avg}\n"
+    f"Greatest increase in Revenue: {greatestIncrease[0]} ${greatestIncrease[1]}\n"
+    f"Greatest decrease in Revenue: {greatestDecrease[0]} ${greatestDecrease[1]}\n"
+)
+
+print(output)
+
+#Write to the text path
+with open(pathout, "w") as txt_file:
+    txt_file.write(output)
+
