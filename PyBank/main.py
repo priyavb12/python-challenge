2ad4a75fedfb46397f0a8c1b0dba7bf220d879b4
Your Name <priyavb@gmail.com>
2024-07-01 22:37:53 -0700

    modified

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,141 +1,54 @@
-import os
-import csv
+import os, csv
+
+def CalculateChange(startingValue, endingValue):
+    return (endingValue - startingValue) #calculating the difference between the ending value and the starting value
 
 
-#analysis_csv = os.path.join('.','resources','budget_data.csv')
-csv_path = os.path.join('resources','budget_data.csv')
-OUTPUT_PATH = os.path.join('analysis','output.txt')
-total_months  = 0
-monthly_change=[]
-net_change_list = []
-greatest_increase = ["", 0]
-greatest_decrease = ["", 999999]
-net_total = 0
+filePath = "C:\\Users\\nprab\\Downloads\\Starter_Code (2)\\Starter_Code\\PyBank\\Resources\\budget_data.csv"
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-with open(csv_path, 'r') as csv_file:
-    csvreader = csv.reader(csv_file, delimiter=',')
-    #next(csvreader)
-    #print(csvreader)
-  
-    header = next(csvreader)
-    first_row = header
-    prev_net = int(csvreader[0][1])
+#filePath = os.path.join('resources', 'budget_data.csv')#import csv block of code
 
-    for row in csvreader:
+with open(filePath) as csvfile: #open the path to the CSV file as a new object
+    csvreader = csv.reader(csvfile, delimiter=',') #pass the csvfile object to a new variable, csvreader
+    csv_header = next(csvreader) #skipping the header so we get the correct count of rows
+    print("Financial Analysis")
+    print("----------------------------")
+    rowCounter, total, avgChange, maxIncreaseAmount, maxIncreaseMonth, minIncreaseAmount, minIncreaseMonth  = 0, 0, 0, 0, 0, 0, 0
+    monthlyValues = [] #declaring empty list to keep track of individual monthly profit/loss values from the CSV
+    monthlyChangeAmount = [] #declaring empty list to keep track of the change in profit/loss across each month
+    monthlyChangeMonth = [] #declaring empty list to keep track of the month corresponding to the amount of the change in profit/loss
 
-        total_months +=1
-        net_total += row[1]
+    for row in csvreader:#Read each row of data after the header
+        monthlyValues.append(row[1]) #appending value of Profit/Loss column of the CSV to the monthlyValues list
+        total += int((row[1])) #calculating total amount of profit/loss over the entire period
+        #passing two values to CalculateChange function. the first parameter, startingValue, will be equal to the previous value of the iterator of the loop 
+        #which would be the current position in the list monthlyValues - 1. the second parameter, endingValue, will be equal to the current value of the 
+        # iterator of the loop. this value is then added to my empty list titled "monthlyChangeAmount"
+        monthlyChangeAmount.append(CalculateChange(int(monthlyValues[rowCounter-1]), int(row[1])))
+        monthlyChangeMonth.append(row[0]) #keep track of the months so I can map the name of the month to the value of the change for that month
+        #keeping track of the row count for two reasons: 1. to be able to get the proper index for my list 
+        #and 2. to count the number of months of data (each month has its own row in the CSV) 
+        rowCounter +=1 
+    #calculating average by taking the sum of the list divided by the length of the list. subtracing one from the length because the value from the first iteration 
+    #of the loop will be discarded since the value is zero as there is no prior value to calculate amount change to month one. rounding result to 2 decimal places.
+    avgChange =  round(sum(monthlyChangeAmount) / (len(monthlyChangeAmount)-1),2) 
+    maxIncreaseAmount = max(monthlyChangeAmount) #maximum value from the monthlyChangeAmount list
+    minIncreaseAmount = min(monthlyChangeAmount) #minimum value from the monthlyChangeAmount list
+    #getting the index of the max value from the Amounts list and using that value as the index for the Months list since they should be 
+    #at the same position since they correspond to the same row in the CSV
+    maxIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(max(monthlyChangeAmount))]
+    minIncreaseMonth = monthlyChangeMonth[monthlyChangeAmount.index(min(monthlyChangeAmount))]
 
-        net_change = row[1] - prev_net
-        prev_net = int(row[1]) # update the new prev net
-        net_change_list += [net_change]
-        monthly_change += [row[0]]
+def GenerateResults(resultType):#function to print the results to terminal or export the results to a text file
+    if (resultType == "print"):#print results to terminal
+        print(f"Total Months: {rowCounter}\nTotal: ${total}\nAverage Change: ${avgChange}\nGreatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})\nGreatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
+    elif(resultType == "file"):#export the same information to a text file
+       
+        #output_path = os.path.join("output", "financialResults.txt")
+        
+        output_path= "C:\\Users\\nprab\\OneDrive\\Desktop\\repos\\python-challenge\\PyBank\\analysis\\output.txt"
+        with open(output_path, 'w') as txtFile:
+            txtFile.write(f"Total Months: {rowCounter}\nTotal: ${total}\nAverage Change: ${avgChange}\nGreatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})\nGreatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
 
-        if net_change > greatest_increase[1]:
-            greatest_increase[0] = row[0]
-            greatest_increase[1] = net_change
-
-        if net_change < greatest_decrease[1]:
-            greatest_decrease[0] = row[0]
-            greatest_decrease[1] = net_change
-
-
-# total_profit_losses = sum(INPUT_PATH.values())
-
-print(f"Total number of months: {total_months}")
-
-
-average_change = sum(net_change_list) / len(net_change_list)
-print(average_change)
-
-# Print the results
-# print("Financial Analysis")
-# print("----------------------------")
-# print(f"Total Months: {total_months}")
-# print(f"Total: ${net_total}")
-# print(f"Average Change: ${average_change:.2f}")
-# print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
-# print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
-
-# # Export the results to a text file
-# with open('financial_analysis.txt', 'w') as output_file:
-#     output_file.write("Financial Analysis\n")
-#     output_file.write("----------------------------\n")
-#     output_file.write(f"Total Months: {total_months}\n")
-#     output_file.write(f"Total: ${net_total}\n")
-#     output_file.write(f"Average Change: ${average_change:.2f}\n")
-#     output_file.write(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n")
-#     output_file.write(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
-
-
-
-
-           
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-total_months = len(csv_path)
-# total_profit_losses = sum(INPUT_PATH.values())
-
-print(f"Total number of months: {total_months}")
-# #print(f"Total Profit/Losses: ${total_profit_losses}")
-
-# changes = []
-# previous_value = None
-
-# for month, value in data.items():
-#     if previous_value is not None:
-#         change = value - previous_value
-#         changes.append(change)
-#     previous_value = value
-
-# # Calculate average of changes
-# average_change = sum(changes) / len(changes)
-
-# #print(f"Changes in Profit/Losses over the entire period: {changes}")
-# #print(f"Average of changes: ${average_change:.2f}")
-
-# max_increase = 0
-# max_increase_month = ""
-# max_decrease = 0
-# max_decrease_month = ""
-# previous_value = None
-
-# for month, value in data.items():
-#     if previous_value is not None:
-#         change = value - previous_value
-#         if change > max_increase:
-#             max_increase = change
-#             max_increase_month = month
-#         if change < max_decrease:
-#             max_decrease = change
-#             max_decrease_month = month
-#     previous_value = value
-
-# analysis = (
-#     f"Total number of months: {total_months}\n"
-#     f"Total Profit/Losses: ${total_profit_losses}\n"
-#     f"Average of changes: ${average_change:.2f}\n"
-#     f"The greatest increase in profits occurred in {max_increase_month} with ${max_increase}"
-#     f"The greatest decrease in profits occurred in {max_decrease_month} with ${max_decrease}")
-# analysis = "test" 
-
-# with open(OUTPUT_PATH,'w') as output_file:
-#     output_file.write(analysis) 
-# print(analysis)
-
-      
-    
+GenerateResults("print")
+GenerateResults("file")       
