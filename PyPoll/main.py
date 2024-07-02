7e7213069751f33b50461db5aa56c719d615f396
Your Name <priyavb@gmail.com>
2024-07-01 22:51:56 -0700

    pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,53 +1,53 @@
-import os
-import csv
+import os, csv
 
+#filePath = os.path.join('Resources', 'election_data.csv')#import csv block of code
 
-#analysis_csv = os.path.join('.','resources','budget_data.csv')
-INPUT_PATH = os.path.join('resources','election_data.csv')
-OUTPUT_PATH = os.path.join('analysis','output.txt')
+filePath = "C:\\Users\\nprab\\OneDrive\\Desktop\\repos\\python-challenge\\PyPoll\\resources\\election_data.csv"
+with open(filePath) as csvfile:#open the path to the CSV file as a new object
+    csvreader = csv.reader(csvfile, delimiter=',') #pass the csvfile object to a new variable, csvreader
+    csv_header = next(csvreader) #skipping the header so we get the correct count of rows
+    initialCandidateList, electionResults = ["Khan", "Correy", "Li", "O'Tooley"], [] #populating list with the initial known candidates. creating new empty list for electionResults.
+    #storing the list of candidate names in a dictionary so I can map vote count to candidate. starting all candidates at 0 votes
+    candidates = { initialCandidateList[0]:0, initialCandidateList[1]:0, initialCandidateList[2]:0, initialCandidateList[3]:0 }
+    rowCounter = 0
 
+    for row in csvreader:#Read each row of data after the header
+        rowCounter+=1 #keep track of number of rows since each row is a vote
+        #if the candidate is not already in initialCandidateList (is a candidate which has not recieved a vote yet) then append this candidate to the list
+        if row[2] not in initialCandidateList:
+            initialCandidateList.append(row[2])
+        if row[2] == initialCandidateList[0]:#if the CSV "Candidate" column for this row matches one of our candidates, give that candidate a vote
+            candidates[initialCandidateList[0]]+=1
+        elif row[2] == initialCandidateList[1]:
+            candidates[initialCandidateList[1]]+=1
+        elif row[2] == initialCandidateList[2]:
+            candidates[initialCandidateList[2]]+=1
+        elif row[2] == initialCandidateList[3]:
+            candidates[initialCandidateList[3]]+=1
 
-os.chdir(os.path.dirname(os.path.realpath(__file__)))
-with open(INPUT_PATH, 'r') as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
-    #print(csvreader)
+    print(f"Election Results\n-------------------------\nTotal Votes: {rowCounter}\n-------------------------")#each row is a vote so row count is equal to the number of votes
 
-total_votes = 0
-candidates = []
-candidate_votes = {}
+    def GenerateResults():#using a function so I can both write to a file and print the results to terminal without having to repeat my code
+        #with dictionaries you cannot look up a key by a value, so using a for loop to map the key and value pairs of the dictionary together 
+        #to allow searching in the opposite direction (searching for a key by a known value)
+        for candidateName, voteCount in candidates.items():
+            #store the candidate name, the percent of the total votes that candidate received, and the actual number of votes that candidate received
+            electionResults.append((f"{candidateName}: {round((voteCount/rowCounter)*100, 1)}% ({voteCount})"))
+            if voteCount == max(candidates[initialCandidateList[0]], candidates[initialCandidateList[1]], candidates[initialCandidateList[2]], candidates[initialCandidateList[3]]):
+                winningCandidate = candidateName #creating a variable to keep track of the winner so I can print that data when I'm outside the for loop
+        #return the results of the election for all the candidates and list the winner.
+        return (f"{electionResults[0]}\n{electionResults[1]}\n{electionResults[2]}\n{electionResults[3]}\n-------------------------\nWinner: {winningCandidate}\n-------------------------")
 
-for row in csvreader:
-        print(row)  
-        total_votes += 1
+def OutputResults(resultType):#function to print the results to terminal or export the results to a text file
+    if (resultType == "print"):#print results to terminal
+        print(GenerateResults())
+    elif(resultType == "file"):#export the same information to a text file
+        #output_path = os.path.join("output", "pollResults.txt")
 
+        output_path = "C:\\Users\\nprab\\OneDrive\\Desktop\\repos\\python-challenge\\PyPoll\\analysis\\electionresults.txt"
+        with open(output_path, 'w') as txtFile:
+            txtFile.write(GenerateResults())
+           # txtFile.write(f"Total Months: {rowCounter}\nTotal: ${total}\nAverage Change: ${avgChange}\nGreatest Increase in Profits: {maxIncreaseMonth} (${maxIncreaseAmount})\nGreatest Decrease in Profits: {minIncreaseMonth} (${minIncreaseAmount})")
 
-    # Loop through each row in the dataset
-   
-     
-
-        # Add candidate to the list if not already in it
-        candidate = row[2]
-        if candidate not in candidates:
-             candidates.append(candidate)
-             candidate_votes[candidate] = 0
-
-        # Count the votes for each candidate
-        candidate_votes[candidate] += 1
-
-# Calculate the percentage of votes each candidate won
-percentages = {candidate: (votes / total_votes) * 100 for candidate, votes in candidate_votes.items()}
-
-# Determine the winner based on the most votes
-winner = max(candidate_votes, key=candidate_votes.get)
-
-# Print the analysis results
-print("Election Results")
-print("-------------------------")
-print(f"Total Votes: {total_votes}")
-print("-------------------------")
-for candidate in candidates:
-    print(f"{candidate}: {percentages[candidate]:.3f}% ({candidate_votes[candidate]})")
-print("-------------------------")
-print(f"Winner: {winner}")
-print("-------------------------")
+OutputResults("print")
+OutputResults("file")
